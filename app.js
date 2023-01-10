ab476697298243d4fd8401895c91b684782dd353
Aayush-047 <88926122+Aayush-047@users.noreply.github.com>
2023-01-10 17:07:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 
-const port=3000;
+const port=process.env.PORT||3000;
 
 //static files
 app.use(express.static('public'));
